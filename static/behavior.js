eb74705537bae790f821420b8e80ad4873527b23
Macbook <admin@admins-MacBook-Air.local>
2016-08-19 17:38:46 -0400

    fixed css/html makes it easier for you to edit css

--- static/behavior.js
+++ static/behavior.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('value').hide()
   $('key').on('click', function(){
     // $('key').toggle()
-    $('value').toggle()
+    $('value').fadeIn('slow').toggle()
   })
   $('right').on('click', function(){
     location.reload(true)
