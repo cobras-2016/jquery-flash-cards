13dd0e3775a0144ca527a2ad2c56a680e97dd6ba
Macbook <admin@admins-MacBook-Air.local>
2016-08-22 10:05:52 -0400

    fixed button, added header

--- static/behavior.js
+++ static/behavior.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
   $('value').hide()
   $('key').on('click', function(){
-    $('value').toggle()
-    $('value').css('color','black')
+    $('value').toggle().css('color','black').css('border-top-color','#4CAF50')
   })
   $('.ph-button').on('click', function(){
     location.reload(true)
